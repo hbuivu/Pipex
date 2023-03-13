f553e2f4e7f36485795c6d414a8efaf1d6bd20ec
Huong Bui Vu <hbui-vu@lab1r6s7.42abudhabi.ae>
2023-03-13 11:06:22 +0400

    3.13.23 wip

--- bonus_heredoc.c
+++ bonus_heredoc.c
@@ -1,4 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus_heredoc.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbui-vu <hbui-vu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 10:11:38 by hbui-vu           #+#    #+#             */
+/*   Updated: 2023/03/13 10:11:38 by hbui-vu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "bonus_pipex.h"
+
+char	*init_str(char *temp, t_mlist *m)
+{
+	char	*str;
+
+	str = (char *)ft_calloc(ft_strlen(temp) + 1, sizeof(char));
+	if (!str)
+		pipex_error(MALLOC_ERR, m, NULL);
+	ft_strlcpy(str, temp, ft_strlen(temp) + 1);
+	free(temp);
+	return (str);
+}
 
 char	*here_doc(t_mlist *m)
 {
@@ -10,21 +34,15 @@ char	*here_doc(t_mlist *m)
 	str = NULL;
 	temp = NULL;
 	new_str = NULL;
-	lim_len = ft_strlen(m->limiter); 
+	lim_len = ft_strlen(m->limiter);
 	while (1)
 	{
 		temp = get_next_line(STDIN_FILENO);
 		if (ft_strncmp(temp, m->limiter, lim_len) == 0)
-			break;
+			break ;
 		if (!str)
-		{
-			str = (char *)ft_calloc(ft_strlen(temp) + 1, sizeof(char));
-			if (!str)
-				pipex_error(MALLOC_ERR, m, NULL);
-			ft_strlcpy(str, temp, ft_strlen(temp) + 1);
-			free(temp);
-		}
-		else 
+			str = init_str(temp, m);
+		else
 		{
 			new_str = ft_strjoin(str, temp);
 			free (str);
@@ -38,8 +56,8 @@ char	*here_doc(t_mlist *m)
 
 int	here_doc_fd(t_mlist *m)
 {
-	int	fd[2];
-	char *str;
+	int		fd[2];
+	char	*str;
 
 	if (pipe(fd) == -1)
 		pipex_error(PIPE_ERR, m, NULL);
